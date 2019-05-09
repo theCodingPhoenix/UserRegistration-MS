d453ea28d1858213d40551c7fd0ec07b6d467434
aakanksha-bhandari <aakanksha.bhandari@gmail.com>
2019-05-09 13:09:50 +0100

    added confirm password validation and email field validation with annotations

--- UserRegistrationProject/Models/UserDetail.cs
+++ UserRegistrationProject/Models/UserDetail.cs
@@ -17,9 +17,18 @@ namespace UserRegistrationProject.Models
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string SecurePassword { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("SecurePassword", ErrorMessage ="Passwords do not match")]
+        public string ConfirmSecurePassword { get; set; }
     }
 }
